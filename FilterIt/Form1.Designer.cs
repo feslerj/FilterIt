50af6805323fd94750761ffb7434c47ed2f4f36d
jfesler <jeffreyfesler@quickenloans.com>
2016-02-03 18:05:39 -0500

    Initial Working Commit

---
+++ FilterIt/Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace FilterIt
+{
+    partial class frmFilterIt
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmFilterIt));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.lstColumns = new System.Windows.Forms.ListBox();
+            this.btnFilterIt = new System.Windows.Forms.Button();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.ddFilters = new System.Windows.Forms.ComboBox();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.lblCurrentFile = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(224, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.saveToolStripMenuItem,
+            this.toolStripSeparator1,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(149, 6);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.DefaultExt = "csv";
+            this.openFileDialog1.Filter = "Comma Seperated Files (*.csv)|*.csv|Excel Files (*.xls, *.xlsx)|*.xls;*.xlsx|All " +
+    "files (*.*)|*.*";
+            // 
+            // lstColumns
+            // 
+            this.lstColumns.FormattingEnabled = true;
+            this.lstColumns.Location = new System.Drawing.Point(12, 53);
+            this.lstColumns.Name = "lstColumns";
+            this.lstColumns.Size = new System.Drawing.Size(200, 95);
+            this.lstColumns.TabIndex = 1;
+            this.lstColumns.SelectedIndexChanged += new System.EventHandler(this.lstColumns_SelectedIndexChanged);
+            // 
+            // btnFilterIt
+            // 
+            this.btnFilterIt.Location = new System.Drawing.Point(12, 195);
+            this.btnFilterIt.Name = "btnFilterIt";
+            this.btnFilterIt.Size = new System.Drawing.Size(75, 23);
+            this.btnFilterIt.TabIndex = 2;
+            this.btnFilterIt.Text = "Filter It!";
+            this.btnFilterIt.UseVisualStyleBackColor = true;
+            this.btnFilterIt.Visible = false;
+            this.btnFilterIt.Click += new System.EventHandler(this.btnFilterIt_Click);
+            // 
+            // ddFilters
+            // 
+            this.ddFilters.FormattingEnabled = true;
+            this.ddFilters.Location = new System.Drawing.Point(12, 154);
+            this.ddFilters.Name = "ddFilters";
+            this.ddFilters.Size = new System.Drawing.Size(200, 21);
+            this.ddFilters.TabIndex = 3;
+            this.ddFilters.Visible = false;
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.saveToolStripMenuItem.Text = "Save";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
+            // 
+            // lblCurrentFile
+            // 
+            this.lblCurrentFile.AutoSize = true;
+            this.lblCurrentFile.Location = new System.Drawing.Point(12, 28);
+            this.lblCurrentFile.Name = "lblCurrentFile";
+            this.lblCurrentFile.Size = new System.Drawing.Size(0, 13);
+            this.lblCurrentFile.TabIndex = 4;
+            // 
+            // frmFilterIt
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(224, 230);
+            this.Controls.Add(this.lblCurrentFile);
+            this.Controls.Add(this.ddFilters);
+            this.Controls.Add(this.btnFilterIt);
+            this.Controls.Add(this.lstColumns);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.Name = "frmFilterIt";
+            this.Text = "Filter It!";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.ListBox lstColumns;
+        private System.Windows.Forms.Button btnFilterIt;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.ComboBox ddFilters;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.Label lblCurrentFile;
+    }
+}
+
---
+++ FilterIt/Form1.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Excel;
+using LumenWorks.Framework.IO.Csv;
+
+namespace FilterIt
+{
+    public partial class frmFilterIt : Form
+    {
+        private DataTable _csvFileData = null;
+        private readonly List<string> _emailEndsWith = new List<string>();
+        private readonly List<string> _addressStartsWith = new List<string>();
+        private string _fileName = String.Empty;
+
+        public frmFilterIt()
+        {
+            InitializeComponent();
+
+            _emailEndsWith.AddRange(ConfigurationManager.AppSettings["EmailEndsWith"].Split('|'));
+            _addressStartsWith.AddRange(ConfigurationManager.AppSettings["AddressStartsWith"].Split('|'));
+
+            ddFilters.Items.Add("PO Boxes");                    //0
+            ddFilters.Items.Add(".edu & .gov Email Addresses"); //1
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var result = openFileDialog1.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                _fileName = openFileDialog1.FileName;
+
+                ddFilters.Visible = btnFilterIt.Visible = false;
+                lstColumns.SelectedIndex = -1;
+                lstColumns.Items.Clear();
+                lblCurrentFile.Text = Path.GetFileName(_fileName);
+                
+                _csvFileData = _fileName.EndsWith(".csv") ? GetDataSetFromCsvFile(_fileName) : GetDataSetFromExcelFile(_fileName);
+
+                foreach (DataColumn column in _csvFileData.Columns)
+                {
+                    lstColumns.Items.Add(column.ColumnName);
+                }
+            }
+        }
+
+        private DataTable GetDataSetFromCsvFile(string filename)
+        {
+            var csvReader = new CachedCsvReader(File.OpenText(filename), true);
+
+            var dt = new DataTable();
+            foreach (var column in csvReader.Columns)
+            {
+                dt.Columns.Add(column.Name);
+            }
+            var headerRow = dt.NewRow();
+            for (int i = 0; i < csvReader.Columns.Count; i++)
+            {
+                headerRow[i] = csvReader.Columns[i].Name;
+            }
+            dt.Rows.Add(headerRow);
+            while (csvReader.ReadNextRecord())
+            {
+                var row = dt.NewRow();
+                for (int i = 0; i < csvReader.FieldCount; i++)
+                {
+                    row[i] = csvReader[i];
+                }
+                dt.Rows.Add(row);
+            }
+            return dt;
+        }
+
+        private DataTable GetDataSetFromExcelFile(string filename)
+        {
+            FileStream stream = File.Open(filename, FileMode.Open, FileAccess.Read);
+
+            // Reading from a binary Excel file ('97-2003 format; *.xls)
+            if (filename.EndsWith(".xls"))
+            {
+                using (IExcelDataReader excelReader = ExcelReaderFactory.CreateBinaryReader(stream))
+                {
+                    return GetDataTableFromExcelReader(excelReader);
+                }
+            }
+                // Reading from a OpenXml Excel file (2007 format; *.xlsx)
+            else
+            {
+                using (IExcelDataReader excelReader = ExcelReaderFactory.CreateOpenXmlReader(stream))
+                {
+                    return GetDataTableFromExcelReader(excelReader);
+                }
+            }
+        }
+
+        private DataTable GetDataTableFromExcelReader(IExcelDataReader excelReader)
+        {
+            var dt = excelReader.AsDataSet().Tables[0];
+            for (int i = 0; i < dt.Columns.Count; i++)
+            {
+                dt.Columns[i].ColumnName = String.IsNullOrEmpty(dt.Rows[0][i].ToString()) ? dt.Columns[i].ColumnName : dt.Rows[0][i].ToString();
+            }
+            return dt;
+        }
+
+        private void lstColumns_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (lstColumns.SelectedIndex != -1)
+            {
+                ddFilters.Visible = btnFilterIt.Visible = true;
+            }
+        }
+
+        private bool ShouldRemoveRow(int selectedColumnIndex, DataRow row, FilterType filterType)
+        {
+            string field = row[selectedColumnIndex].ToString().ToLower();
+
+            switch (filterType)
+            {
+                case FilterType.FilterByAddress:
+                    return _addressStartsWith
+                        .Select(filterStr => filterStr.ToLower())
+                        .Any(field.StartsWith);
+                case FilterType.FilterByEmail:
+                    return _emailEndsWith
+                        .Select(filterStr => filterStr.ToLower())
+                        .Any(field.EndsWith);
+            }
+
+            return false;
+        }
+
+        private void btnFilterIt_Click(object sender, EventArgs e)
+        {
+            if (_csvFileData == null || ddFilters.SelectedIndex <= -1 || lstColumns.SelectedIndex <= -1)
+                return;
+
+            var removedRows = new List<DataRow>();
+            var filteredCsvFileData = new DataTable();
+            filteredCsvFileData.Columns.AddRange(_csvFileData.Columns.OfType<DataColumn>().Select(col => new DataColumn(col.ColumnName)).ToArray());
+
+            foreach (DataRow row in _csvFileData.Rows)
+            {
+                if (ShouldRemoveRow(lstColumns.SelectedIndex, row, (FilterType)ddFilters.SelectedIndex))
+                {
+                    removedRows.Add(row);
+                }
+                else
+                {
+                    filteredCsvFileData.ImportRow(row);
+                }
+            }
+
+            MessageBox.Show(string.Format("Records Found To Be Removed {0}", removedRows.Count));
+
+            SaveRecords(string.Concat(_fileName, "_removed_", DateTime.Now.Ticks, ".csv"), removedRows);
+
+            _csvFileData = filteredCsvFileData;
+        }
+
+        private void SaveRecords(string filename, IEnumerable<DataRow> removedRows)
+        {
+            using (var sw = new StreamWriter(filename))
+            {
+                foreach (var row in removedRows)
+                {
+                    sw.WriteLine(WriteCsvLine(row));
+                }
+                sw.Close();
+            }
+        }
+
+        private string WriteCsvLine(DataRow row)
+        {
+            var build = new StringBuilder();
+            foreach (var field in row.ItemArray)
+            {
+                string strField = field.ToString();
+                if (strField.Contains(",") || strField.Contains("\""))
+                {
+                    strField = strField.Replace("\"", "\"\"");
+                    strField = string.Concat("\"", strField, "\"");
+                }
+                build.Append(strField);
+                build.Append(",");
+            }
+            build.Remove(build.Length - 1, 1);
+            return build.ToString();
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string filteredFileName = string.Concat(_fileName, "_filtered_", DateTime.Now.Ticks, ".csv");
+            SaveRecords(filteredFileName, _csvFileData.Rows.OfType<DataRow>());
+
+            MessageBox.Show(string.Format("Saved as {0}!", filteredFileName));
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+
+    public enum FilterType
+    {
+        FilterByAddress = 0,
+        FilterByEmail = 1,
+    }
+}
